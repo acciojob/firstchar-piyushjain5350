5076568fa6d5d3654b3f06623ac05c101b864027
piyushjain5350 <126599802+piyushjain5350@users.noreply.github.com>
2023-08-01 22:04:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function firstChar(text) {
   // your code here
+	return text.charAt(0).trim();
 }
 
-// Do not change the code below
+// Do not change the code below 
 
 const text = prompt("Enter text:");
 alert(firstChar(text));
